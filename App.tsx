016bf7eecc92f615be580ab7ec25c4fa849fd072
ptdien <ptdien.lxag@gmail.com>
2021-10-22 18:31:44 +0700

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export const App = () => {
+  return (
+    <div className="p-2">
+      <h1>Hutbot Interview</h1>
+    </div>
+  );
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-starter-typescript-vbjxue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-starter-typescript-vbjxue)
